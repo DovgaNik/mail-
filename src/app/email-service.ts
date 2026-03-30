ae69e6ec81bd40ec35d0869390bc3985ba9f2ffc
DovgaNik <nikita.dovgan.05@hotmail.com>
2026-03-30 11:15:28 +0200

    feat: made a request to get domains list

--- src/app/email-service.ts
+++ src/app/email-service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class EmailService {}
+export class EmailService {
+  private baseUrl: string = "https://api.mail.gw";
+
+  constructor(private http: HttpClient) {}
+
+  setupAccount(): Observable<any> {
+
+    return this.http.get<any>(this.baseUrl + "/domains")
+
+  }
+}
